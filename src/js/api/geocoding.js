478c903b485bad4e6ef15bc2400dbab1c1253f98
Rolens1 <67217126+Rolens1@users.noreply.github.com>
2025-04-07 11:56:32 -0400

    geoloc, getCoords working

--- src/js/api/geocoding.js
+++ src/js/api/geocoding.js
@@ -1,13 +1,12 @@
 export async function getCoordinates(city) {
-    // const apiKey = 'b412e0fef649413ca0c4a63c9d5af09e';
-    // const res = await fetch(`https://api.opencagedata.com/geocode/v1/json?q=${encodeURIComponent(city)}&key=${apiKey}`);
-    // const data = await res.json();
-    // if (!data.results || data.results.length === 0) throw new Error("Ville non trouvée.");
-    // return data.results[0].geometry;
-
-    return {
-        lat: 48.8555,
-        lon: 2.3522,
-        city: city
-    }
+  const apiKey = "b412e0fef649413ca0c4a63c9d5af09e";
+  const res = await fetch(
+    `https://api.opencagedata.com/geocode/v1/json?q=${encodeURIComponent(
+      city
+    )}&key=${apiKey}`
+  );
+  const data = await res.json();
+  if (!data.results || data.results.length === 0)
+    throw new Error("Ville non trouvée.");
+  return data.results[0].geometry;
 }
--- src/js/main.js
+++ src/js/main.js
@@ -1,33 +1,38 @@
 import { getCoordinates } from "./api/geocoding.js";
-import { getImage } from "./api/pixabay.js"
+import { getImage } from "./api/pixabay.js";
 
-const cityInput = document.querySelector("#search_input")
-const btnSearch = document.querySelector("#btn_search")
+const cityInput = document.querySelector("#search_input");
+const btnSearch = document.querySelector("#btn_search");
+const formSearch = document.getElementById("form_search");
 
-btnSearch.addEventListener("click", async () => {
-    const cityName = cityInput.value.trim()
-    console.log(cityName)
-    if (cityInput.value.trim() === "") {
-        console.log("Not Working")
-    }
+// console.log("Go");
 
-    try {
-        const coords = await getCoordinates(cityName)
-        console.log("Coords :", coords)
-        renderStory(cityName)
-    } catch (error) {
-        console.log("Error geoloc:", error)
-    }
-})
+formSearch.addEventListener("submit", async (e) => {
+  e.preventDefault();
 
+  const cityName = cityInput.value.trim();
+  console.log(cityName);
 
-async function renderStory(city, containerId = 'main_story') {
-    const image = await getImage(city);
-    const container = document.getElementById(containerId);
-  
-    container.innerHTML = `
+  if (!cityName) {
+    console.log("Veuillez entrer ville");
+    return;
+  }
+
+  try {
+    const coords = await getCoordinates(cityName);
+    console.log(coords);
+  } catch (error) {
+    console.log("Erreur geo loc", error);
+  }
+});
+
+async function renderStory(city, containerId = "main_story") {
+  const image = await getImage(city);
+  const container = document.getElementById(containerId);
+
+  container.innerHTML = `
       <div class="story">
         <h2>${city}</h2>
-        ${image ? `<img src="${image}" alt="${city}">` : ''}
+        ${image ? `<img src="${image}" alt="${city}">` : ""}
     `;
-  }
+}
